3f479337b6039ed7faa1df6d261a5655a7403ab6
Olivia Wen <oliviaw7@ad.unc.edu>
2022-11-17 15:39:32 -0500

    rearranged server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ import { roll } from "./lib/roll.js"
 import minimist from "minimist"
 import express from "express"
 
-const app = express(); 
+var app = express(); 
 app.use(express.urlencoded({extended: true}))
 
-const args = minimist(process.argv.slice(2))
+var args = minimist(process.argv.slice(2))
 
 var port = 5000
 
@@ -18,15 +18,13 @@ if ("port" in args) {
     port = args.port
 }
 
-app.use(function(req, res) {
-    res.send("404 NOT FOUND")
-})
-
-
 app.get('/app/', (req, res) => {
     res.send("200 OK")
 })
 
+app.use(function(req, res) {
+    res.send("404 NOT FOUND")
+})
 
 app.get('/app/roll/', (req, res) => {
     res.send(roll(sides, dice, rolls))
